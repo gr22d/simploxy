61c4559f4faa6837a408e5eac5a512ba62323230
gr22d <57122609+gr22d@users.noreply.github.com>
2022-02-04 17:42:17 +0900

    Update options.js

--- options.js
+++ options.js
@@ -1,4 +1,11 @@
 const browserAppData = this.browser || this.chrome;
+const defaultIcon = 'default-64.png';
+
+function stopProxy() {
+  var config = {mode: "direct"};
+  chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+}
+
 function saveOptions(e) {
   browserAppData.storage.local.set({
 	proxy_server: document.querySelector('#proxy_server').value,
@@ -6,18 +13,21 @@ function saveOptions(e) {
 	popup_flag: document.querySelector('#popup_flag').checked
   }, () => {
     const status = document.querySelector('.status');
-    status.textContent = '저장되었습니다.';
+    status.textContent = 'Saved!!';
+	stopProxy();
+	browserAppData.browserAction.setIcon({path: { 19: 'icons/' + defaultIcon } });
     setTimeout(() => {
       status.textContent = '';
     }, 1000);
   });
   e && e.preventDefault();
 }
+
 function restoreOptions() {
   browserAppData.storage.local.get({
 	proxy_server: '127.0.0.1',
 	proxy_port: 8080,
-	popup_flag: true
+	popup_flag: false
   }, items => {
     document.querySelector('#proxy_server').value = items.proxy_server;
     document.querySelector('#proxy_port').value = items.proxy_port;
